dd5da5b706ed5f35a3438b66fba190f8d6fd581f
BB5014 <bblabriz@bbox.fr>
2019-11-18 17:02:59 +0100

    setting up work environment, add bootstrap.Building application architecture and components identity, stack

---
+++ src/components/myPresentation/Identity.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Identity = () => (
+	<div className="w-25 d-flex flex-column m-3 p-3">
+		<h1 className="d-flex justify-content-center">Nathalie BALOTTE</h1>
+		<h3 className="d-flex justify-content-center">
+			Développeuse Web Junior
+		</h3>
+
+		<a
+			className="nav-link"
+			href="https://github.com/BB5014?tab=repositories"
+		>
+			<img
+				alt="myPhoto"
+				src="../assets/myphoto.png"
+				className="w-25 m-auto p-2 d-flex justify-content-center"
+			/>
+		</a>
+		<p>
+			En reconvesion professionnelle, j'ai décidé de suivre la formation
+			intensive de la Wild Code School de Bidart, pour devenir
+			Développeuse Web & mobile. Après 5 mois intensifs d'apprentissage,
+			je cherche un poste pour vous apportez mes compétences.
+		</p>
+
+		<hr className="w-100 " />
+	</div>
+);
+
+export default Identity;
---
+++ src/components/myPresentation/StackSkills.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Stacks = () => (
+	<div className="w-25 d-flex flex-column m-3">
+		<h4>STACK SKILLS</h4>
+		<ul>
+			<li>HTML 5, CSS 3, JS, ReactJs</li>
+			<li>Bootstrap, Reactstrap</li>
+			<li>NodeJs, Express, MySql, SQL, Mocha</li>
+			<li>
+				Linux, Ubuntu, Git, VsCode, GitHub      
+				<img alt="github" src="../assets/github.png" className="pl-2"/>
+			</li>
+			<li>Trello</li>
+		</ul>
+	</div>
+);
+export default Stacks;
