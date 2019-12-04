5461c34dbfa6afe3cfe0622e9987a7d26321359d
BB5014 <bblabriz@bbox.fr>
2019-12-04 12:27:15 +0100

    update component Identity with props

--- src/App.js
+++ src/App.js
@@ -9,14 +9,24 @@ import Contact from './components/myPresentation/Contact';
 
 function App() {
   return (
-    <div className="App d-flex flex-column flex-fill border-blue">
-      <Identity />
-      <Stacks />
-      <Soft />
-      <Education />
-      <CenterInterest />
-      <Contact />
-    </div>
+		<div className="App d-flex flex-column flex-fill border-blue">
+			<Identity
+				firstName="Nathalie"
+				name="Balotte"
+				job="Développeuse Web Junior"
+				photo="../../assets/myphoto.png"
+				presentation="	En reconvesion professionnelle, j'ai décidé de suivre la
+				formation intensive de la Wild Code School de Bidart, pour
+				devenir Développeuse Web & mobile. Après 5 mois intensifs
+				d'apprentissage, je cherche un poste pour vous apportez mes
+				compétences."
+			/>
+			<Stacks />
+			<Soft />
+			<Education />
+			<CenterInterest />
+			<Contact />
+		</div>
   );
 }
 
